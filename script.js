8a3fd63bdcaf8dd2998a994b007ed02d1816aa0a
Lorna London <lornalondon109@hotmail.co.uk>
2023-07-19 12:40:37 +0100

    layout completed

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector('.form');
+const amount = document.querySelector('.amount');
+const total = document.querySelector('.total-input');
+const people = document.querySelector('.people-input')
