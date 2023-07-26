489766bd175d97ff93c34917b4f55ac3e5e12a78
Lorna London <lornalondon109@hotmail.co.uk>
2023-07-26 15:41:24 +0100

    website app main features finished; only form validation to complete and ios styling

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-const form = document.querySelector('.form');
-const amount = document.querySelector('.amount');
-const total = document.querySelector('.total-input');
-const people = document.querySelector('.people-input')
+
+const root = document.querySelector(':root');
+let moon = document.querySelector('.moon-icon');
+let amount = document.querySelector('.amount');
+let total = document.querySelector('.total-input');
+let people = document.querySelector('.people-input')
+
+let calculate = document.querySelector('.calc-btn');
+
+calculate.addEventListener('click', function(e) {
+e.preventDefault();
+amount.textContent = '£';
+  let billAmount = total.value / people.value;
+  let decimalBillAmount = billAmount.toFixed(2); 
+    amount.append(decimalBillAmount);
+    console.log(decimalBillAmount);
+});
+
+moon.addEventListener('click', function() {
+  root.style.setProperty('--themecolor', 'magenta'), moon.addEventListener('click', function() {
+  root.style.setProperty('--themecolor', 'chartreuse'), moon.addEventListener('click', function() {
+    root.style.setProperty('--themecolor', 'orange'), moon.addEventListener('click', function() {
+      root.style.setProperty('--themecolor', 'deepskyblue'), moon.addEventListener('click', function() {
+        root.style.setProperty('--themecolor', 'blanchedalmond') , moon.addEventListener('click', function() {
+        root.style.setProperty('--themecolor', 'cyan')
+         }) 
+        })
+      })
+    })
+  })
+})
+
+  
